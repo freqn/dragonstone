7521874b6e24de3a3d2a01c86cb8f6a42125d09d
Chip Eyler <ceyler@gmail.com>
2014-01-21 01:51:08 -0500

    testing routes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,7 +29,7 @@ LearnRails::Application.configure do
   config.action_mailer.smtp_settings = {
 address: "smtp.gmail.com",
 port: 587,
-domain: ENV["freqn.com"],
+domain: ENV["DOMAIN_NAME"],
 authentication: "plain",
 enable_starttls_auto: true,
 user_name: ENV["GMAIL_USERNAME"],
@@ -39,9 +39,7 @@ password: ENV["GMAIL_PASSWORD"]
   config.action_mailer.default_url_options = { :host =>'localhost:3000' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = true
-  
-  
-  
+     
   # Send email in development mode.
 config.action_mailer.perform_deliveries = true
 end
