fdeea0f170e396b04d784ff1da990863d573d5bd
Chip Eyler <ceyler@gmail.com>
2014-02-08 09:41:53 -0500

    update application layout

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -2,6 +2,8 @@ class VisitorsController < ApplicationController
 
 	def new
 		@owner = Owner.new
+		flash.now[:notice] = "You've arrived!"
+		flash.now[:alert] = 'My birdthay is soon.'
 	end
 	
 end
